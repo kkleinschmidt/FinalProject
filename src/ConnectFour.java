c23876e04cfb09912b2ad42e3862cd69b5a9dfe9
Kaili Kleinschmidt <kkleinschmidt@luc.edu>
2017-12-08 15:29:30 +0000

    updated main
    Signed-off-by: Kaili Kleinschmidt <kkleinschmidt@luc.edu>

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -4,22 +4,30 @@ public class ConnectFour {
 
   // important variables
   int round = 0;
-  boolean gameType = false;
 
   // data structures
-  Map<Integer, Stack<String>> board = new HashMap<Integer, Stack<String>>();
-  Stack<Integer> column1 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column2 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column3 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column4 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column5 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column6 = new Stack<String>(" ", " ", " ", " ", " ", " ");
-  Stack<Integer> column7 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Map<Integer, Stack<String>> board;
+  Stack<Integer> column1;
+  Stack<Integer> column2;
+  Stack<Integer> column3;
+  Stack<Integer> column4;
+  Stack<Integer> column5;
+  Stack<Integer> column6;
+  Stack<Integer> column7;
 
-  public ConnectFour(boolean gameType) {
-    this.gameType = gameType;
+  // initialize
+  public ConnectFour() {
+    board = new HashMap<Integer, Stack<String>>();
+    column1 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column2 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column3 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column4 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column5 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column6 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+    column7 = new Stack<String>(" ", " ", " ", " ", " ", " ");
   }
 
+  // fills out the HashMap
   public HashMap<Integer, Stack<String>> buildBoard() {
     board.put(1, column1);
     board.put(2, column2);
@@ -32,7 +40,8 @@ public class ConnectFour {
     return board;
   }
 
-  public Stack<String> buildColumn(int col, String item) {
+  // adds a piece to a column
+  public void buildColumn(int col, String item) {
     if (col == 1) {
       column1.push(item);
     } else if (col == 2) {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+package src;
+
 import java.util.*;
 
 public class Main {
 
   public static void main(final String[] args) {
 
+    boolean gameType = true;
     final Scanner input = new Scanner(System.in);
+
+    // connect 4 game begins
+    ConnectFour c = new ConnectFour();
   }
 }
