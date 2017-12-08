01fe6af72499f6c192a5675218fdeba089faa468
Kaili Kleinschmidt <kkleinschmidt@luc.edu>
2017-12-08 15:16:07 +0000

    added buildColumn method
    Signed-off-by: Kaili Kleinschmidt <kkleinschmidt@luc.edu>

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -7,20 +7,20 @@ public class ConnectFour {
   boolean gameType = false;
 
   // data structures
-  HashMap<Integer, Stack<Integer>> board = new HashMap<Integer, Stack<Integer>>();
-  Stack<Integer> column1 = new Stack<Integer>();
-  Stack<Integer> column2 = new Stack<Integer>();
-  Stack<Integer> column3 = new Stack<Integer>();
-  Stack<Integer> column4 = new Stack<Integer>();
-  Stack<Integer> column5 = new Stack<Integer>();
-  Stack<Integer> column6 = new Stack<Integer>();
-  Stack<Integer> column7 = new Stack<Integer>();
+  Map<Integer, Stack<String>> board = new HashMap<Integer, Stack<String>>();
+  Stack<Integer> column1 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column2 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column3 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column4 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column5 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column6 = new Stack<String>(" ", " ", " ", " ", " ", " ");
+  Stack<Integer> column7 = new Stack<String>(" ", " ", " ", " ", " ", " ");
 
   public ConnectFour(boolean gameType) {
     this.gameType = gameType;
   }
 
-  public HashMap<Integer, Stack<Integer>> buildBoard() {
+  public HashMap<Integer, Stack<String>> buildBoard() {
     board.put(1, column1);
     board.put(2, column2);
     board.put(3, column3);
@@ -28,5 +28,31 @@ public class ConnectFour {
     board.put(5, column5);
     board.put(6, column6);
     board.put(7, column7);
+
+    return board;
   }
+
+  public Stack<String> buildColumn(int col, String item) {
+    if (col == 1) {
+      column1.push(item);
+    } else if (col == 2) {
+      column2.push(item);
+    } else if (col == 3) {
+      column3.push(item);
+    } else if (col == 4) {
+      column4.push(item);
+    } else if (col == 5) {
+      column5.push(item);
+    } else if (col == 6) {
+      column6.push(item);
+    } else if (col == 7) {
+      column7.push(item);
+    }
+  }
+
+  // public void printBoard(HashMap<Integer, Stack<String>> b){
+  //   for(int i = 0; i < b.size(); i++){
+  //     Stack<String> x = b.get(i);
+  //   }
+  // }
 }
