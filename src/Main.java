5fc62a907ecea3e1a6d0a57044e29a7350dc2867
Kaili Kleinschmidt <kkleinschmidt@luc.edu>
2017-12-08 15:48:18 +0000

    add init in WordScramble & work on Main
    Signed-off-by: Kaili Kleinschmidt <kkleinschmidt@luc.edu>

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,24 @@ public class Main {
     boolean gameType = true;
     final Scanner input = new Scanner(System.in);
 
-    // connect 4 game begins
-    ConnectFour c = new ConnectFour();
+    // CALL INPUT
+
+    // if(input == "no"){
+    //   gameType = false;
+    // } else if(input == "yes"){
+    //   gameType = true;
+    // } else{
+    //   System.out.println("Please answer yes or no:");
+    //   //CALL INPUT AGAIN
+    // }
+    if (gameType) {
+      // connect 4 game begins
+      ConnectFour c = new ConnectFour();
+      c.buildBoard();
+    }
+
+    if (!gameType) {
+      WordScramble w = new WordScramble();
+    }
   }
 }
--- src/WordScramble.java
+++ src/WordScramble.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class WordScramble {
   // important variables
   int round = 0;
-  List<String> words = new Arraylist<String>();
+  List<String> words;
+
+  public WordScramble() {
+    words = new Arraylist<String>();
+  }
 }
